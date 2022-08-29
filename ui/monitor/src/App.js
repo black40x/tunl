4928e6597a09833d7c9fcb290c15e6fe828e4593
DmVsk <dm.vsk@yandex.ru>
2022-08-29 23:49:35 +0400

    monitor reconnect

--- ui/monitor/src/App.js
+++ ui/monitor/src/App.js
@@ -10,18 +10,30 @@ function App() {
     const [filterStr, setFilterStr] = useState('')
     const [currentRequest, setCurrentRequest] = useState(undefined)
     const [connectStatus, setConnectStatus] = useState(false)
+    let connectInterval = undefined
 
     const connectToLocal = () => {
         let ws = new WebSocket(`ws://localhost:${localPort}/connect`)
         ws.onopen = function(evt) {
             setConnectStatus(true)
+
+            if (connectInterval !== undefined) {
+                clearInterval(connectInterval)
+            }
         }
         ws.onclose = function(evt) {
             ws = null
             setConnectStatus(false)
+            tryToConnect()
         }
         ws.onmessage = function(evt) {
             setRequests(o => [JSON.parse(evt.data), ...o])
+        }
+    }
+
+    const tryToConnect = () => {
+        if (!connectInterval) {
+            connectInterval = setInterval(connectToLocal, 1000)
         }
     }
 
